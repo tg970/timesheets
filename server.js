0bf332c8cd171c0e04e57ebc70f0d20d45e9e852
tg970 <tgoodman@outlook.com>
2017-12-02 14:33:34 -0600

    server setup

---
+++ server.js
@@ -0,0 +1,46 @@
+const express  = require('express');
+const session  = require('express-session');
+const mongoose = require('mongoose');
+const morgan   = require('morgan');
+const bcrypt   = require('bcrypt');
+const app      = express();
+const PORT     = 3000;
+
+// connect to database
+const mongoURI = 'mongodb://localhost:27017/timesheet_app';
+mongoose.connect(mongoURI, { useMongoClient: true});
+mongoose.Promise = global.Promise;
+
+// test db connection
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message));
+db.on('connected', () => console.log('Mongo running: ', mongoURI));
+
+// Middleware
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(session({
+	 secret: "kickasstakenames",
+	 resave: false,
+	 saveUninitialized: false
+}));
+
+// Controllers
+const sessionController = require('./controllers/session.js');
+const timesheetController = require('./controllers/timesheet.js');
+const homeController = require('./controllers/home.js')
+
+app.use('/user', sessionsController);
+app.use('/timesheet', timesheetController);
+app.use('/home', homeController);
+
+
+// root route
+app.get('/', (req, res) => res.redirect('/home'));
+
+
+// :ear
+app.listen(PORT, () => {
+  console.log('TimeSheets: ', PORT);
+});
