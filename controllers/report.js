78d6c522de566f4200e7d887009307789f78b556
tg970 <tgoodman@outlook.com>
2017-12-07 14:38:31 -0600

    basic report page set up

--- controllers/report.js
+++ controllers/report.js
@@ -2,10 +2,28 @@
 const express = require('express');
 const router  = express.Router();
 const TimeSheet   = require('../models/timesheet');
+const User   = require('../models/users');
 
 // Reports page landing
-router.get ('/', (req, res) => {
-   res.render('pages/report.ejs')
+router.get('/', async (req, res) => {
+   const userArr = await User.find();
+   res.render('pages/report.ejs', {
+      user: req.session,
+      userArr: userArr,
+      dataArr: []
+   })
+});
+
+router.post('/run', async (req, res) => {
+   console.log('body:', req.body);
+   const userArr = await User.find();
+   const dataArr = await TimeSheet.find(req.body)
+   //console.log(dataArr);
+   res.render('pages/report.ejs', {
+      user: req.session,
+      userArr: userArr,
+      dataArr: dataArr
+   })
 });
 
 
